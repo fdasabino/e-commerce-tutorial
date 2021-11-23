f08f32631da7e53d485a8260d0416f31276fe103
Francisco Sabino <Francisco Sabino>
2021-11-23 15:42:45 +0100

    added category tests

--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -8,7 +8,9 @@ def create_admin_user(django_user_model):
     Return admin user for tests
 
     """
-    return django_user_model.objects.create_superuser("admin", "a@a.com", "password")
+    return django_user_model.objects.create_superuser(
+        "admin", "a@a.com", "password"
+    )
 
 
 @pytest.fixture(scope="session")
@@ -18,3 +20,4 @@ def db_fixture_setup(django_db_setup, django_db_blocker):
     """
     with django_db_blocker.unblock():
         call_command("loaddata", "db_admin_fixture.json")
+        call_command("loaddata", "db_category_fixture.json")
